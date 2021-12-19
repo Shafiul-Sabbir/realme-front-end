a7b3a4c77fde865b7f0145a84e022175dcc9c3c3
Shafiul-Sabbir <sabbirvai82@gmail.com>
2021-12-19 23:52:27 +0600

    changes

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaUOy73PDP16Sc5gcDxyfPAJ8hdj32bkc",
+  authDomain: "realme-f1447.firebaseapp.com",
+  projectId: "realme-f1447",
+  storageBucket: "realme-f1447.appspot.com",
+  messagingSenderId: "588021714483",
+  appId: "1:588021714483:web:cf64997aee44f452c3dd46"
+};
+export default firebaseConfig;
